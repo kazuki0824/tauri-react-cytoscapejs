84a5e29cb9687d7b349263d2d7c2b2d4c028bc39
maleicacid <maleicacid824+dev@gmail.com>
2024-06-06 09:57:50 +0900

    Update

--- fresh.config.ts
+++ fresh.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 
 export default defineConfig({
+  build: {
+    outDir: "./dist",
+  },
+  server: {
+    port: 8000,
+  },
   plugins: [tailwind()],
 });
